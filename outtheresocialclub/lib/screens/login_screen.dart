e6a819c51619184e0fc63cc82b14ac401f80a42d
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2024-10-13 00:13:41 -0700

    make changes to login form based on mockup

--- outtheresocialclub/lib/screens/login_screen.dart
+++ outtheresocialclub/lib/screens/login_screen.dart
@@ -53,53 +53,132 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Login")),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
+      backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+      appBar: AppBar(
+        title: const Text("Login"),
+        backgroundColor:
+            Color.fromARGB(255, 255, 255, 255), // AppBar background color
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          // Scrollable content to avoid overflow
           child: Column(
-            mainAxisAlignment:
-                MainAxisAlignment.start, // Center content vertically
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Logo at the top-center of the screen
-              Center(
-                child: Image.asset(
-                  'lib/assets/logos/OTSC_Logo_Horizontal_Black.png', // Path to your logo
-                  width: 300, // Set the width of the logo
-                  height: 300, // Set the height of the logo
-                  fit: BoxFit.contain,
+              // Logo with 300x300 size
+              Image.asset(
+                'lib/assets/logos/OTSC_Logo_Horizontal_FullColor.png',
+                width: 300,
+                height: 300,
+                fit: BoxFit.contain,
+              ),
+              const SizedBox(height: 0), // Spacing between logo and form
+              Padding(
+                // Card containing the login form
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Card(
+                  color: Color.fromRGBO(255, 255, 255, 1),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  elevation: 5.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          // Email input field
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Email',
+                              border: OutlineInputBorder(),
+                            ),
+                            validator: (value) {
+                              if (value!.isEmpty) return 'Enter your email';
+                              return null;
+                            },
+                            onSaved: (value) => _username = value!,
+                          ),
+                          const SizedBox(height: 20), // Spacing between fields
+
+                          // Password input field
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Password',
+                              border: OutlineInputBorder(),
+                            ),
+                            obscureText: true,
+                            validator: (value) {
+                              if (value!.isEmpty) return 'Enter your password';
+                              return null;
+                            },
+                            onSaved: (value) => _password = value!,
+                          ),
+                          const SizedBox(
+                              height: 20), // Spacing before login button
+
+                          // Login button
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: _login,
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color.fromRGBO(
+                                    44, 44, 44, 1), // Black button
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                              ),
+                              child: const Text(
+                                'Log in',
+                                style: TextStyle(
+                                    color: Color.fromRGBO(255, 255, 255, 1)),
+                              ),
+                            ),
+                          ),
+
+                          // Forgot password link
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: TextButton(
+                              onPressed: () {
+                                // Handle forgot password action
+                              },
+                              child: const Text(
+                                'Forgot password?',
+                                style: TextStyle(
+                                  color: Color.fromRGBO(44, 44, 44, 1),
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
               ),
-              const SizedBox(height: 30), // Spacing between the logo and the form
-
-              // Username input field
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Username'),
-                validator: (value) {
-                  if (value!.isEmpty) return 'Enter your username';
-                  return null;
-                },
-                onSaved: (value) => _username = value!,
-              ),
-
-              // Password input field
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Password'),
-                obscureText: true,
-                validator: (value) {
-                  if (value!.isEmpty) return 'Enter your password';
-                  return null;
-                },
-                onSaved: (value) => _password = value!,
-              ),
-
-              const SizedBox(height: 20), // Spacing before the login button
-
-              // Login button
-              ElevatedButton(
-                onPressed: _login,
-                child: const Text("Login"),
+              // "Don't have an account? Apply today" link
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Don't have an account? "),
+                  GestureDetector(
+                    onTap: () {
+                      // Handle apply today action
+                    },
+                    child: const Text(
+                      'Apply today',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
